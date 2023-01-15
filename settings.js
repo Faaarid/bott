43588ffe72f279b86a9969b0c08175db2fdcfa7d
Faaarid <90858028+Faaarid@users.noreply.github.com>
2023-01-15 07:07:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -27,24 +27,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "Archie-Bot" //namabot kalian
+global.ownername= "Archie" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['263785170909'] //premium user
-global.owner = ["263785170909","263772602990"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "263785170909" // nomor wa kalian
-global.ownernomerr = "+263785170909" //nmr wa kalian
+global.premium = ['263734408604'] //premium user
+global.owner = ["263734408604","263772602990"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "263734408604" // nomor wa kalian
+global.ownernomerr = "+263734408604" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Archie' //sticker wm ubah
+global.author = 'Bot WhatsApp ' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
